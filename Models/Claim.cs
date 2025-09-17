b88d5395e61c9e342c2e9b651847b3b584e85189
Mmasebotsana Nkosi <ST10452632@imconnect.edu.za>
2025-09-17 17:46:35 +0200

    Added Claim model with basic fields

---
+++ Models/Claim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ContractMonthlyClaimSystem.Models
+{
+    public class Claim
+    {
+        public int ClaimId { get; set; }
+        public string Month { get; set; }    //e.g "September 2025"
+        public decimal TotalAmount { get; set; }  //e.g: 4500.00
+        public string Status{ get; set; }   //submitted/approved
+        public DateTime SubmittedAt { get; set; }
+       
+    }
+}
